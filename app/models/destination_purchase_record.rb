07c198cc27ce3f438d2c9cd152c7f690b30c7805
jukayamashita <113895719+jukayamashita@users.noreply.github.com>
2022-10-31 17:44:16 +0900

    修正１

---
+++ app/models/destination_purchase_record.rb
@@ -0,0 +1,25 @@
+class DestinationPurchaseRecord
+  include ActiveModel::Model
+  attr_accessor :user_id, :product_id, :zip_code, :prefecture_id, :city, :address_1, :address_2, :phone_number, :token
+
+  with_options presence: true do
+    # purchase_record
+    validates :user_id
+    validates :product_id
+    # destination
+    validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :address_1
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+
+    # トークン
+    validates :token, presence: true
+  end
+
+  def save
+    purchase_record = PurchaseRecord.create(user_id: user_id, product_id: product_id)
+    Destination.create(purchase_record_id: purchase_record.id, zip_code: zip_code, prefecture_id: prefecture_id, city: city,
+                       address_1: address_1, address_2: address_2, phone_number: phone_number)
+  end
+end
